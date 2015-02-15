6d85ed0428ad43a277de687d62e016ee85121bfc
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2015-02-16 00:30:51 +0100

    implement translation (germany), fix user have cardboard condition

--- www/js/app.js
+++ www/js/app.js
@@ -32,7 +32,34 @@ app.config(function ($translateProvider) {
     $translateProvider.translations("en", {
         WELCOME: {
             DESCRIPTION: "Das Google Cardboard ist eine Virtuell Reality–Brille aus Pappe. In Kombination mit einem Smartphone wird das Cardboard zur ultimativen Virtuell Reality Expierience. Durch die von Google genutzten bikonvexe – Linsen und der Nutzung spezieller Apps, wird der Bildschirm des Phones zu einem faszinierenden, dreidimensionalen VR-Display.",
-            AMAZONBUTTON: "Cardboard auf Amazon"
+            AMAZONBUTTON: "Cardboard auf Amazon",
+            NEXTBUTTON: "Weiter",
+            USERHAVECARDBOARDBUTTON: "Ich habe ein Cardboard."
+        },
+        PANORAMASCREEN: {
+            WAITDIALOG: "Bitte warten..."
+        },
+        SETTINGS: {
+            SMOOTHADJUSTMENT: "Smooth Einstellung",
+            COMPASSADJUSTMENT: "Kompass Einstellung",
+            OWNPANORAMABUTTON: "Eigenes Panorama wählen",
+            ONLINEPANORAMASBUTTON: "Online herunterladen",
+            AMAZONBUTTON: "Cardboard bei Amazon bestellen"
+        },
+        ONLINEPANORAMA: {
+            PICTUREONETITLE: "Golden Gate Bridge",
+            PICTUREONESUBTITLE: "San Francisco",
+            PICTURETWOTITLE: "Europabrücke",
+            PICTURETWOSUBTITLE: "Deutschland",
+            PICTURETHREETITLE: "In Game",
+            PICTURETHREESUBTITLE: "Panorama",
+            PICTUREFOURTITLE: "Düsseldorf Bilk",
+            PICTUREFOURSUBTITLE: "Deutschland",
+            PICTUREFIVETITLE: "Wildkogel",
+            PICTUREFIVESUBTITLE: "Kitzbüheler Alpen",
+            PICTURESIXTITLE: "Skyline",
+            PICTURESIXSUBTITLE: "Münschen",
+            BACKBUTTON: "Zurück"
         }
     });
 
@@ -46,7 +73,9 @@ app.config(function ($translateProvider) {
 
 app.run(function ($document, $rootScope, pictureDirectionService, languageService) {
     $document.on("deviceready", function () {
-        //languageService.setGermanIfAvailable();
-        pictureDirectionService.initialize();
+        if (intel.xdk.isxdk !== true) {
+            languageService.setGermanIfAvailable();
+            pictureDirectionService.initialize();
+        }
     });
 });
