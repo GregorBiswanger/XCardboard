0704fbd719813172f41f67b07b978082d2b3ae05
pherting <pher@live.de>
2015-02-16 01:33:35 +0100

    About Window

--- www/js/controllers/settingsController.js
+++ www/js/controllers/settingsController.js
@@ -10,6 +10,10 @@ var settingsController = function ($scope, settingsService, ownPictureService) {
         intel.xdk.device.launchExternal("http://www.amazon.de/gp/product/B00NF8PO4G/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=B00NF8PO4G&linkCode=as2&tag=doblne-21&linkId=AJGHZL57FNTE6DMQ");
     };
 	
+	$scope.onNavigateExternURLTouched = function(url) {
+		intel.xdk.device.launchExternal(url);
+	};
+	
 	$scope.onOwnPanoramaTouched = function() {
 		ownPictureService.getOwnPicture();
 		$scope.isSettingsPanelOpen = false;
