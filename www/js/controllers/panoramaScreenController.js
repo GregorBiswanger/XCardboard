2d5396f0ed56b2e9bab157034767be9f7ef1d7c7
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2015-02-20 00:33:59 +0100

    fix 360 degre bug

--- www/js/controllers/panoramaScreenController.js
+++ www/js/controllers/panoramaScreenController.js
@@ -2,6 +2,7 @@ var panoramaScreenController = function ($scope, $rootScope, settingsService) {
     $scope.isImageLoading = true;
     $scope.picturePosition = 0;
     $scope.settingsService = settingsService;
+    $scope.imageWidth = 0;
 
     $scope.$on("picturePositionChanged", function (sender, position) {
         $scope.$apply(function () {
@@ -15,37 +16,39 @@ var panoramaScreenController = function ($scope, $rootScope, settingsService) {
 
     $scope.panoramaStyle = function () {
 
-		var image = new Image();
-		image.onload = function() {
-			return {
-				height: window.innerHeight + "px",
-				backgroundPosition: $scope.picturePosition + 'px 470px',
-				backgroundImage: "url(" + settingsService.panoramaImage + ")",
-				backgroundSize: this.width + "px " + this.height + "px",
-				transition: settingsService.smoothAdjustment + "s ease",
-				transform: "translate3d(0, 0, 0)"
-			};
-		}
+        var image = new Image();
+        image.onload = function () {
+            $scope.imageWidth = image.width;
+        };
 
-		image.src = settingsService.panoramaImage;
+        image.src = settingsService.panoramaImage;
 
         return {
             height: window.innerHeight + "px",
-            backgroundPosition: $scope.picturePosition + 'px 470px',
+            backgroundPosition: "-" + $scope.picturePosition + 'px 490px',
             backgroundImage: "url(" + settingsService.panoramaImage + ")",
-            backgroundSize: "4625px 774px",
-			transition: settingsService.smoothAdjustment + "s ease",
-			transform: "translate3d(0, 0, 0)"
+            backgroundSize: $scope.imageWidth + "px 774px",
+            transition: getTransition(),
+            transform: "translate3d(0, 0, 0)"
         };
     };
+
+    function getTransition() {
+        if ($scope.picturePosition <= 50 && $scope.picturePosition >= 0 ||
+            $scope.picturePosition <= $scope.imageWidth && $scope.picturePosition >= ($scope.imageWidth - 50)) {
+            return "none";
+        } else {
+            return settingsService.smoothAdjustment + "s linear";
+        }
+    }
 
     $scope.onScreenTouched = function () {
         $rootScope.$broadcast("panoramaScreenTouched", true);
     };
-	
-	$scope.$on("ownPictureChanged", function(value) {
-		$scope.$apply();
-	});
+
+    $scope.$on("ownPictureChanged", function (value) {
+        $scope.$apply();
+    });
 };
 
 panoramaScreenController.$inject = ["$scope", "$rootScope", "settingsService"];
